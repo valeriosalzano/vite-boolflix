601545d6b3ad62917d99d11b97c51a5962d74871
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-31 18:40:19 +0200

    added stars with precise value

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,50 +5,55 @@
     <h5>{{ cardInfo.original_title }}</h5>
     <div> <img :src="`https://flagsapi.com/${this.cardInfo.language.toUpperCase()}/flat/64.png`" :alt="this.cardInfo.language"> </div>
     <div>{{ cardInfo.stars }}</div>
+    <CardStars :vote_value="this.cardData.vote_average"/>
 
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'card',
-    props: {
-      cardData: Object,
-      chose: String,
-    },
-    computed:{
-      cardInfo()
-      {
-        let card = {};
-        switch(this.chose) {
-          case 'movie':
-            return card = {
-              title : this.cardData.title,
-              original_title : this.cardData.original_title,
-              language : this.languageFix(this.cardData.original_language),
-              stars : this.voteFix(this.cardData.vote_average),
-            };
+import CardStars from './CardStars.vue';
 
-          default: return console.log('computed:cardInfo !')
+  export default {
+    name: "card",
+    components: {
+      CardStars,
+    },
+    props: {
+        cardData: Object,
+        chose: String,
+    },
+    computed: {
+        cardInfo() {
+            let card = {};
+            switch (this.chose) {
+                case "movie":
+                    return card = {
+                        title: this.cardData.title,
+                        original_title: this.cardData.original_title,
+                        language: this.languageFix(this.cardData.original_language),
+                        stars: this.voteFix(this.cardData.vote_average),
+                    };
+                default: return console.log("computed:cardInfo !");
+            }
         }
-      }
     },
     methods: {
-      voteFix(vote){
-        return Math.ceil(vote/2);
-      },
-      languageFix(language){
-        switch(language){
-          case 'en':
-            return 'gb';
-          case 'ja':
-            return 'jp';
-          default:
-            return language;
+        voteFix(vote) {
+            return Math.ceil(vote / 2);
+        },
+        languageFix(language) {
+            switch (language) {
+                case "en":
+                    return "gb";
+                case "ja":
+                    return "jp";
+                default:
+                    return language;
+            }
         }
-      }
-    }
-  }
+    },
+    components: { CardStars }
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/CardStars.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card-stars">
+    <div class="stars-container">
+      <div class="empty-stars">
+        <i v-for="star in 5" class="fa-regular fa-star"></i>
+      </div>
+      <div class="solid-stars" :style="`width:${vote_value*10}%`">
+        <i v-for="star in 5" class="fa-solid fa-star"></i>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'card-stars',
+    props: {
+      vote_value: Number,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.stars-container {
+  position: relative;
+  display: inline-block;
+}
+.solid-stars {
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  white-space: nowrap;
+}
+</style>
